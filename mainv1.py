0b7cad63cd4dd408aded809011de2195cf913272
Vizo <mecoc1011@gmail.com>
2024-09-28 21:31:43 +0530

    something

--- mainv1.py
+++ mainv1.py
@@ -30,28 +30,23 @@ def home():
     session.clear()
     if request.method == "POST":
         name = request.form.get("name")
-        code = request.form.get("code")
-        join = request.form.get("join", False)
-        create = request.form.get("create", False)
 
         if not name:
-            return render_template("home.html", error="Please enter a name.", code=code, name=name)
+            return render_template("home.html", error="Please enter a name.", name=name)
 
-        if join != False and not code:
-            return render_template("home.html", error="Please enter a room code.", code=code, name=name)
-        
-        room = code
-        if create != False:
-            room = generate_unique_code(4)
-            rooms[room] = {"members": 0, "messages": []}
-        elif code not in rooms:
-            return render_template("homev1.html", error="Room does not exist.", code=code, name=name)
-        
-        session["room"] = room
         session["name"] = name
-        return redirect(url_for("room"))
+        return redirect(url_for("join"))
 
     return render_template("homev1.html")
+
+
+@app.route("/join", methods=["POST", "GET"])
+def join():
+    name = session.get("name")
+    if not name:
+        return redirect(url_for("home"))
+    return render_template("join.html", name=name)
+
 
 @app.route("/room")
 def room():
